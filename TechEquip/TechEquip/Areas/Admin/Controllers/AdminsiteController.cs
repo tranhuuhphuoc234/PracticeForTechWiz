d446116bb3b329a34f609955e5d2e52a025269c4
phước <phuoc.th18@aptechlearning.edu.vn>
2021-04-20 15:34:45 +0700

    Create Project

---
+++ TechEquip/TechEquip/Areas/Admin/Controllers/AdminsiteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechEquip.Areas.Admin.Controllers
+{
+    public class AdminsiteController : Controller
+    {
+        // GET: Admin/Adminsite
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
